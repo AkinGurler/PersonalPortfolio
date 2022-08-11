d99fa8b757c47c42ad381c4101eb92b0922e511a
Akin GURLER <akingurler98@hotmail.com>
2022-08-11 17:25:57 +0300

    floating nav component css and active animation with usestate created and navbar finished

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
-import {FaHome} from 'react-icons/fa'
-import {FaUser} from 'react-icons/fa'
-import {FaBook} from 'react-icons/fa'
-import {RiServiceFill} from 'react-icons/ri'
-import {AiFillContacts} from 'react-icons/ai'
+import { FaHome } from 'react-icons/fa'
+import { FaUser } from 'react-icons/fa'
+import { FaBook } from 'react-icons/fa'
+import { RiServiceFill } from 'react-icons/ri'
+import { AiFillContacts } from 'react-icons/ai'
 
-const nav = () => {
-  return (
-    <nav>
-    <a href="#"><FaHome/></a>
-    <a href="#about"><FaUser/></a>
-    <a href="#experience"><FaBook/></a>
-    <a href="#services"><RiServiceFill/></a>
-    <a href="#contact"><AiFillContacts/></a>
-    </nav>
-  )
+const Nav = () => {
+    const [active, setActive] = useState('')
+    return (
+        <nav>
+            <a href="#"
+                onClick={() => setActive('#')}
+                className={active === '#' ? 'active' : ''}><FaHome /></a>
+            <a href="#about"
+                onClick={() => setActive('#about')}
+                className={active === '#about' ? 'active' : ''}
+            ><FaUser /></a>
+            <a href="#experience"
+                onClick={() => setActive('#experience')}
+                className={active === '#experience' ? 'active' : ''}
+            ><FaBook /></a>
+            <a href="#services"
+                onClick={() => setActive('#services')}
+                className={active === '#services' ? 'active' : ''}
+            ><RiServiceFill /></a>
+            <a href="#contact"
+                onClick={() => setActive('#contact')}
+                className={active === '#contact' ? 'active' : ''}
+            ><AiFillContacts /></a>
+        </nav>
+    )
 }
 
-export default nav
+
+
+export default Nav
