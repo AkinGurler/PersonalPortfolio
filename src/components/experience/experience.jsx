1eb1edc138e3b50d52b0cc3cd83942e77ff3537e
Akin GURLER <akingurler98@hotmail.com>
2022-08-13 01:15:37 +0300

    experience component ready for styling

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './experience.css'
+import {GiProgression} from 'react-icons/gi'
 
-const experience = () => {
+
+const FRONT_SKILLS = [
+   {name: "HTML", level:"Intermediate"},
+    {name: "CSS", level:"Intermediate"} ,
+    {name: "Bootstrap", level:"Beginner"} ,
+    {name: "JavaScript", level:"Intermediate"} ,
+    {name: "React", level:"Intermediate"} ,
+  
+];
+const BACKEND_SKILLS = [
+  {name: "NodeJS", level:"Beginner"},
+   {name: "MYSQL", level:"Intermediate"} ,
+   {name: "POSTGRESQL", level:"Intermediate"} ,
+   {name: "MONGODB", level:"Beginner"} ,
+   
+ 
+];
+
+const Experience = () => {
+  const [frontSkills, setFrontSkills] = useState(FRONT_SKILLS)
+  const [backSkills, setBackSkills] = useState(BACKEND_SKILLS)
+
+
+ 
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>what Skill i Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            {frontSkills.map((skill,index)=>(
+               <article key={index} className="experience__details">
+                <GiProgression/>
+                <h4>{skill.name}</h4>
+                <small className='text-light'>{skill.level}</small>
+               </article>
+            ))}
+          </div>
+          
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+        {backSkills.map((skill,index)=>(
+               <article key={index} className="experience__details">
+                <GiProgression/>
+                <h4>{skill.name}</h4>
+                <small className='text-light'>{skill.level}</small>
+               </article>
+            ))}
+            </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default experience
+export default Experience
