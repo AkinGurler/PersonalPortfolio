d9e54d454d81d1c81fdcb5c9531a94eb55eb9668
Akin GURLER <akingurler98@hotmail.com>
2022-08-14 21:28:56 +0300

    portfolio compenent finished

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './portfolio.css'
+import image1 from '../../assets/portfolio1.jpg'
+import image2 from '../../assets/portfolio2.jpg'
+import image3 from '../../assets/portfolio3.jpg'
 
-const portfolio = () => {
-  return (
-    <section id='portfolio' >portfolio</section>
-  )
+const PROJECT_DATA = [
+    {
+        img: image1, title: 'Web Fitness',
+        github: "https://github.com/AkinGurler/WebFitness",
+        livedemo: "https://fitnessclubdemo.netlify.app/"
+    },
+    {
+        img: image2, title: 'Web Fitness',
+        github: "https://github.com/AkinGurler/WebFitness",
+        livedemo: "https://fitnessclubdemo.netlify.app/"
+    },
+    {
+        img: image3, title: 'Web Fitness',
+        github: "https://github.com/AkinGurler/WebFitness",
+        livedemo: "https://fitnessclubdemo.netlify.app/"
+    }
+]
+
+const Portfolio = () => {
+    const [projects, setProjects] = useState(PROJECT_DATA)
+    const [frontSkills, setFrontSkills] = useState(PROJECT_DATA)
+
+    return (
+        <section id='portfolio' >
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <container className="portfolio__container">
+                {projects.map((item, index) => (
+                    <article key={index} className='portfolio__item'>
+                        <div className="portfolio__item-image">
+                            <img src={item.img} alt="" />
+                        </div>
+                        <h3>{item.title}</h3>
+                        <div className="portfolio__item-cta">
+                            <a href={item.github} className='btn'>Github</a>
+                            <a href={item.livedemo} className='btn btn-primary'>Live Demo</a>
+                        </div>
+
+                    </article>
+                ))}
+
+            </container>
+        </section>
+    )
 }
 
-export default portfolio
+export default Portfolio
