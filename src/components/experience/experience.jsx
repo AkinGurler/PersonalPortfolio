d128527bab72bace447cffe609f4b06f1ec1351c
Akin GURLER <akingurler98@hotmail.com>
2022-08-14 20:48:23 +0300

    Experience.js completed

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import './experience.css'
-import {GiProgression} from 'react-icons/gi'
+import { GiProgression } from 'react-icons/gi'
 
 
 const FRONT_SKILLS = [
-   {name: "HTML", level:"Intermediate"},
-    {name: "CSS", level:"Intermediate"} ,
-    {name: "Bootstrap", level:"Beginner"} ,
-    {name: "JavaScript", level:"Intermediate"} ,
-    {name: "React", level:"Intermediate"} ,
-  
+  { name: "HTML", level: "Intermediate" },
+  { name: "CSS", level: "Intermediate" },
+  { name: "Bootstrap", level: "Beginner" },
+  { name: "JavaScript", level: "Intermediate" },
+  { name: "React", level: "Intermediate" },
+
 ];
 const BACKEND_SKILLS = [
-  {name: "NodeJS", level:"Beginner"},
-   {name: "MYSQL", level:"Intermediate"} ,
-   {name: "POSTGRESQL", level:"Intermediate"} ,
-   {name: "MONGODB", level:"Beginner"} ,
-   
- 
+  { name: "NodeJS", level: "Beginner" },
+  { name: "MYSQL", level: "Intermediate" },
+  { name: "POSTGRESQL", level: "Intermediate" },
+  { name: "MONGODB", level: "Beginner" },
+
+
 ];
 
 const Experience = () => {
@@ -25,37 +25,41 @@ const Experience = () => {
   const [backSkills, setBackSkills] = useState(BACKEND_SKILLS)
 
 
- 
+
   return (
     <section id='experience'>
       <h5>what Skill i Have</h5>
-      <h2>My Experience</h2>
+      <h2 >My Experience</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            {frontSkills.map((skill,index)=>(
-               <article key={index} className="experience__details">
-                <GiProgression/>
-                <h4>{skill.name}</h4>
-                <small className='text-light'>{skill.level}</small>
-               </article>
+            {frontSkills.map((skill, index) => (
+              <article key={index} className="experience__details">
+                <GiProgression className='experience__details-icon'/>
+                <div className='experience__properties'>
+                  <h4>{skill.name}</h4>
+                  <small className='text-light'>{skill.level}</small>
+                </div>
+              </article>
             ))}
           </div>
-          
+
         </div>
         <div className="experience__backend">
-        <h3>Backend Development</h3>
+          <h3>Backend Development</h3>
           <div className="experience__content">
-        {backSkills.map((skill,index)=>(
-               <article key={index} className="experience__details">
-                <GiProgression/>
-                <h4>{skill.name}</h4>
+            {backSkills.map((skill, index) => (
+              <article key={index} className="experience__details">
+                <GiProgression className='experience__details-icon'/>
+                <div className='experience__properties'>
+                  <h4 className='experience__name'>{skill.name}</h4>
                 <small className='text-light'>{skill.level}</small>
-               </article>
+                </div>
+              </article>
             ))}
-            </div>
+          </div>
         </div>
       </div>
     </section>
