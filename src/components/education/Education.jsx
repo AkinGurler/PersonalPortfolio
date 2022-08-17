597898b3a9dede37d74e75bac5fb93a8ce6851d6
Akin GURLER <akingurler98@hotmail.com>
2022-08-17 20:12:37 +0300

    Education component created and finished

---
+++ src/components/education/Education.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import './education.css'
+import { Chrono } from "react-chrono";
+
+const Education = () => {
+    const items = [{
+        title: "2023",
+        cardTitle: "Istanbul University-Cerrahpaşa",
+        url: "https://www.iuc.edu.tr/tr/_",
+        cardSubtitle: "Undergraduated",
+        cardDetailedText: "Computer Engineering-GPA=3.29",
+
+    }, {
+        title: "2023",
+        cardTitle: "Istanbul University-Cerrahpaşa",
+        url: "https://www.iuc.edu.tr/tr/_",
+        cardSubtitle: "Undergraduated",
+        cardDetailedText: "Computer Engineering-GPA=3.29"
+    }];
+
+    return (
+        <section id='education' >
+            <h2>My Education </h2>
+            <div className="container education__container">
+                <div className="education__schools">
+                    <div className="education__school">
+                        <h4>Istanbul <br />University</h4>
+                        <small className='text-light'>2023 Istanbul</small>
+                    </div>
+                    <div className="education__school">
+                        <h4>Recep Güngör<br /> Anatolian<br /> High School</h4>
+                        <small className='text-light'>2018 Istanbul</small>
+                    </div>
+
+
+                </div>
+                <div className="timeline">
+                    <div className="education__results">
+                        <div className="education__result__container">
+                        <div className="education__result__content">
+                            <h3>Undergraduate</h3>
+                            <small className='text-light'>GPA=3.29</small>
+                        </div>
+                    </div>
+
+
+                    <div className="education__result__container">
+                        <div className="education__result__content">
+                            <h3>High School<br /> Diploma</h3>
+                            <small className='text-light'>GPA=3.29</small>
+                        </div>
+                    </div>
+                    </div>
+                    
+
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Education
